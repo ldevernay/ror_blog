91826a594a237fba80b6b335dc6b15dda66a55ef
ldevernay <boblehamster@yahoo.fr>
2017-06-05 11:00:50 +0200

    articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+Article.create(title: 'Premier article', text:'Vraiment tres interessant')
+Article.create(title: 'Second article', text:'Pas mieux')
